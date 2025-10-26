0dd798084d3e9551a2e7cd31993f8cfc028bb7a1
Abdul Wahab <awahab271203@gmail.com>
2025-10-26 12:58:09 +0700

    tambahan

--- resources/views/rotinan/nurzati.blade.php
+++ resources/views/rotinan/nurzati.blade.php
@@ -8,11 +8,13 @@
     <h3 class="fw-bold mb-4">Sholawat Nurzati</h3>
 
     <p dir="rtl" class="fs-2 lh-lg" style="font-family: 'Scheherazade New', serif; line-height: 2.3; font-size: 2rem;">
-        اَللّٰهُمَّ صَلِّ وَسَلِّمْ وَبَارِكْ عَلَى سَيِّدِنَا مُحَمَّدٍ النُّورِ الذَّاتِي وَالسِّرِّ السَّارِي
+        اَللّٰهُمَّ صَلِّ وَسَلِّمْ وَبَارِكْ عَلَى سَيِّدِنَا مُحَمَّدٍ النُّورِ الذَّاتِي
+        <br/> وَالسِّرِّ السَّارِي
         فِي سَائِرِ الأَسْمَاءِ وَالصِّفَاتِ وَعَلَى أَلِهِ وَصَحْبِهِ وَسَلِّمْ
     </p>
 
-    <div class="mt-5 text-start" style="max-width: 800px; margin: 0 auto;">
+    {{-- Jarak lebih jauh ke bawah --}}
+    <div class="text-start" style="max-width: 900px; margin: 100px auto 0;">
         <p class="fw-bold">Artinya:</p>
         <p>
             "Ya Allah berikanlah rahmat, keselamatan, dan berkah kepada junjungan kami Nabi Muhammad SAW
@@ -21,4 +23,6 @@
         </p>
     </div>
 </div>
+    <div class="text-start" style="max-width: 900px; margin: 100px auto 0;">
+
 @endsection
